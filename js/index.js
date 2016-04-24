c78e1d062b443b016e55bc87cf0105a649e256e2
PREETAM KUMAR <PREETAM KUMAR>
2016-04-24 20:18:25 +0530

    First Commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+	$("#myCarousel").carousel("pause");
+	$("#myCarousel").carousel("cycle");
+	$("#myCarousel").carousel({interval:100});
+	$(".accordian").onclick(function(){
+		//$("#answerwhyait").classList.toggle("show");
+	});
+	
+/*
+		$("#button3").popover({title:"Programs",content:"aman",placement:"bottom"});
+	
+	
+	$(".item1").click(function(){
+		$("#myCarousel").carousel(0);
+	});
+	$(".item2").click(function(){
+		$("#myCarousel").carousel(1);
+	});
+	$(".item3").click(function(){
+		$("#myCarousel").carousel(2);
+	});
+	*/
+
+
+
+});
